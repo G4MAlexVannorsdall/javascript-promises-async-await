02e74cceeedd16ace4104237fa6a2ee316313cbd
G4MAlexVannorsdall <alexandria.vannorsdall@gear4music.com>
2022-09-28 08:45:45 +0100

    part 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+require ('movies.json') = movies;
+import fetchWithoutTimeout from './services' ;
+
 export function fetchMovies() {
-    
+    const resolveFunction = () => movies;
+
+    fetchWithoutTimeout(1000).then(resolveFunction);
+
+    const moviePromise = fetchMovies();
+
+    console.log (moviePromise.then(results));
 }
