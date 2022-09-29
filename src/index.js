fa99571d6d6a85c1185eec9fc5c0e3700329fad8
G4MAlexVannorsdall <alexandria.vannorsdall@gear4music.com>
2022-09-29 13:23:06 +0100

    again part 3

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import {fetchWithTimeout} from './services' ;
 const movies = require('./data/movies.json');
 
 export function fetchMovies() {
-
+    const resolveFunction = () => movies;
+    
     return fetchWithTimeout(1000).then(resolveFunction);
 }
 
 const moviePromise = fetchMovies();
-const resolveFunction = () => movies;
+
 
 moviePromise.then(results => {
     console.log(results);
