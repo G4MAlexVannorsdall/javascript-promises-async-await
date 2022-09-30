440c40afc45e1c9f8f1a04411e71544e8405d674
G4MAlexVannorsdall <alexandria.vannorsdall@gear4music.com>
2022-09-30 14:06:17 +0100

    Worked on 4

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import {fetchWithTimeout} from './services' ;
 const movies = require('./data/movies.json');
 
 export function getBooksAndMovies() {
-    Promise.all([fetchBooks(), fetchMovies()]).then(([books, movies ]) => ({
+     return Promise.all([fetchBooks(), fetchMovies()]).then(([books, movies ]) => ({
         books,
         movies
       }))
       .catch(error => console.log("Error fetching books and movies", error));
-
 }
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+
+return getBooksAndMoviesPromise.then(results () => {
+    console.log('getBooksAndMoviesPromise', results);
+});
